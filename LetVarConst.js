d8ffcf448689f3d9836839be0b42297ae331ce0d
Piyush Maheshwari <asawapiyush20@gmail.com>
2023-10-29 20:10:36 +0530

    Const_Error

--- LetVarConst.js
+++ LetVarConst.js
@@ -1,1 +1,7 @@
-Console.log("Heelo")
+const accountID = 1121232332
+let accountEmail = "pimaheshwari@gmail.com"
+var accPassword = 12334567
+accountCity = "Jaipur"
+
+// accountID = 2
+console.log(accountID)
