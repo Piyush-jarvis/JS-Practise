066443d925a0f5728d1ed52c1bfddd82dd7df8e4
Piyush Maheshwari <asawapiyush20@gmail.com>
2023-11-16 21:13:02 +0530

    Conversion_1

--- ConversioOperation.js
+++ ConversioOperation.js
@@ -1,6 +1,36 @@
-let score = "33"
+let score = "33abc"
 
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueinnumber = Number(score)
-console.log(typeof valueinnumber)
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);// as the string value is converted to number 
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1
+//false => 0
+//null => 0
+//undefined => NaN
+/*
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); // true
+
+*/
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // false
+
+// let isLoggedIn = "piyush"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //true
+
+// let isLoggedIn = " "
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // true
